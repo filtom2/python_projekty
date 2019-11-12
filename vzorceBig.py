9bb931965eb9fdad18dd217d333ba911e29f7c88
filtom2 <milanpoltak254@hotmail.com>
2019-11-12 23:11:30 +0100

    Zrusenie moznosti pre volby pokracovania vo vypocte

--- vzorceBig.py
+++ vzorceBig.py
@@ -5,18 +5,19 @@ odpoved1 = input('Zvolte si utvar:')
 
 if odpoved1 == '1':
     print('Vybrali ste si rovinny utvar znamy pod nazvom trojuholnik.')
-    trojuholnikOdpoved1 = input(
-        'Prajete si pokracovat a prejst tak na vypocty ?     a/n     ')
-    if trojuholnikOdpoved1 == 'a':
-        print('Zadajte strany :')
-        trojuholnikA = int(input('Zadajte rozmery strany a: '))
-        trojuholnikB = int(input('Zadajte rozmery strany b: '))
-        trojuholnikC = int(input('Zadajte rozmery strany c: '))
-        if trojuholnikA + trojuholnikB > trojuholnikC and trojuholnikB + trojuholnikC > trojuholnikA and trojuholnikC + trojuholnikA > trojuholnikB:
-            print('Trojuholnik sa da narysovat.')
-            if trojuholnikC ** 2 == trojuholnikB ** 2 + trojuholnikA ** 2:
-                print('Trojuholnik je pravouhly.')
-            else:
-                print('Trojuholnik neni pravouhly.')
+    print('Zadajte strany :')
+    trojuholnikA = int(input('Zadajte rozmery strany a: '))
+    trojuholnikB = int(input('Zadajte rozmery strany b: '))
+    trojuholnikC = int(input('Zadajte rozmery strany c: '))
+    if trojuholnikA + trojuholnikB > trojuholnikC and trojuholnikB + trojuholnikC > trojuholnikA and trojuholnikC + trojuholnikA > trojuholnikB:
+        print('Trojuholnik sa da narysovat.')
+        if trojuholnikC ** 2 == trojuholnikB ** 2 + trojuholnikA ** 2:
+            print('Trojuholnik je pravouhly.')
         else:
-            print('Neda sa narysovat.')
+            print('Trojuholnik neni pravouhly.')
+    else:
+        print('Neda sa narysovat.')
+
+if odpoved1 == '2':
+    print('Vybrali ste si rovinny utvar znamy pod nazvom stvoruholnik.')
+    print('Zadajte strany :')
