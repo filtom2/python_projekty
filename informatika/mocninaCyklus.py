90ad4850fafabefca71882c8bd15ee20022a50c8
filtom2 <milanpoltak254@hotmail.com>
2019-12-12 08:26:31 +0100

    initial commit

---
+++ informatika/mocninaCyklus.py
@@ -0,0 +1,12 @@
+#Milan Seliga II.F
+#vypocet mocnin
+n = int(input('Vlozte cislo: '))
+mocnina = int(input('Vlozte na ktoru mocninu: '))
+
+
+cislo = 1
+
+for i in range(1, mocnina + 1):  #urcuje opakovanie
+    cislo = n * cislo
+
+print('Vas vysledok je,', cislo)
