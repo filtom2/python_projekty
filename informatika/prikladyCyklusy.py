56dbcddf142a6a1ab9b5b7dce924c6f34c4660bc
filtom2 <milanpoltak254@hotmail.com>
2019-12-12 08:38:51 +0100

    pridany aritmeticky priemer

--- informatika/prikladyCyklusy.py
+++ informatika/prikladyCyklusy.py
@@ -3,16 +3,17 @@
 n = int(input('Vlozte cislo: '))
 suma = 0
 medzivysledky = input('Prajete si medzivysledky ? y/n ')
+
 if medzivysledky == 'y':
     for i in range(1,n+1):
         suma = suma + i
         print(suma)
-    print('Vysledne cislo je:',suma)
+    print('Vysledne cislo je:',suma,'Priemer je,',suma/n)
 
 elif medzivysledky == 'n':
     for i in range(1,n+1):
         suma = suma + i
-    print(suma)
+    print(suma,'A priemer je,', suma/n)
 
 else:
     print('Chyba spustite este raz.')
