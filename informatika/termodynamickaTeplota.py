a64514aecb0dbe95e0538351a5e213446bd8bbf3
filtom2 <milanpoltak254@hotmail.com>
2019-12-16 13:07:01 +0100

    Pridany kod, uz funkcne

--- informatika/termodynamickaTeplota.py
+++ informatika/termodynamickaTeplota.py
@@ -1,3 +1,8 @@
 #Milan Seliga II.F
 #vypocet tabulky
-cislo = 1
+
+vstup = int(input('Zadajte zaciatocnu cislo: '))
+koniec = int(input('Zadajte koncovu teplotu: '))
+kroky = int(input('Zadajte po kolko chcete zvacsovat: '))
+for i in range(vstup - 1, koniec + 1, kroky):
+    print(i, i + 273)
