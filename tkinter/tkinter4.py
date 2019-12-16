9d024fc3f0faa94d47264cf98edf11a3f89b2c69
filtom2 <milanpoltak254@hotmail.com>
2019-12-16 12:50:21 +0100

    Initial commits

---
+++ tkinter/tkinter4.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+
+def printName(event):
+    print('Hello my name is Program.')
+
+
+button_1 = Button(root, text='Print name')
+button_1.bind("<Button-1>", printName)
+button_1.grid(row=1, column=1)
+
+root.mainloop()
