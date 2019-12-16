b8738b63ef7285a9666b7d16d0f8e8004c208d0c
filtom2 <milanpoltak254@hotmail.com>
2019-12-16 12:51:28 +0100

    Nic nezmenene

--- informatika/mocninaCyklus.py
+++ informatika/mocninaCyklus.py
@@ -7,6 +7,6 @@ mocnina = int(input('Vlozte na ktoru mocninu: '))
 cislo = 1
 
 for i in range(1, mocnina + 1):  #urcuje opakovanie
-    cislo = n * cislo
+    cislo = n * cislo               
 
 print('Vas vysledok je,', cislo)
