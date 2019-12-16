76372f2e57e2999f18011e2d537ec6dea5c9162b
filtom2 <milanpoltak254@hotmail.com>
2019-12-16 13:27:42 +0100

    Pridane formatovanie

--- informatika/termodynamickaTeplota.py
+++ informatika/termodynamickaTeplota.py
@@ -4,5 +4,8 @@
 vstup = int(input('Zadajte zaciatocnu cislo: '))
 koniec = int(input('Zadajte koncovu teplotu: '))
 kroky = int(input('Zadajte po kolko chcete zvacsovat: '))
+
+print('Stupne Celzia:           Kelviny: ')
+print('       ----------------------------')
 for i in range(vstup - 1, koniec + 1, kroky):
-    print(i, i + 273)
+    print('{:11} {:20}'.format(i,i + 273))
