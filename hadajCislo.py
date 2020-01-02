c941c5077f669dbffe286a1c7da5cab6298941da
filtom2 <milanpoltak254@hotmail.com>
2020-01-02 17:21:45 +0100

    zjednodusenie

--- hadajCislo.py
+++ hadajCislo.py
@@ -1,5 +1,6 @@
 #hrac musi zistit cislo ktore pocitac vygeneruje od 0 - 100 a potom hrac hlada a pocitac mu radi viac/menej
-from random import randint                #importuje sa kniznica ktora obsahuje iba prikaz randint aby sa setrila pamat kedze ostatne programy sa nevyuziju
+from random import randint 
+import time             #importuje sa kniznica ktora obsahuje iba prikaz randint aby sa setrila pamat kedze ostatne programy sa nevyuziju
 n = randint(0,100)              #vygeneruje nadhodne cislo od 0 do 100
 hadanie = int(input('Hadajte nahodne cislo od 0-100: \n'))             #uzivatelov vstup kde hada cislo
 while n != 'guess':         #TODO pridanie hori prihorieva atp.   #program je v loope pretoze sa musi hadat stale a musi to byt v cykle az pokial sa cislo neuhadne
@@ -7,6 +8,7 @@ while n != 'guess':         #TODO pridanie hori prihorieva atp.   #program je v 
         hadanie = int(input('Menej, hadaj dalej \n'))
     elif hadanie < n:
         hadanie = int(input('Viacej, hadaj dalej \n'))
-    elif hadanie == n:
+    else:
         print('Uhadol si cislo.')
+        time.sleep(1)
         break                            #Ukonci while cyklus
