20a26582e76090e2dd5289fd3b896d49709d6e77
filtom2 <milanpoltak254@hotmail.com>
2020-01-13 13:25:10 +0100

    pridany pocet pokusov

--- informatika/hadanie.py
+++ informatika/hadanie.py
@@ -1,5 +1,6 @@
 from random import *
 from time import sleep
+pokusy = 1
 
 n = randint(0,100)
 guess = int(input('Hadaj cislo od 0 do 100: '))
@@ -7,6 +8,8 @@ guess = int(input('Hadaj cislo od 0 do 100: '))
 while n != guess:
     if n < guess:
         guess = int(input('Hadaj menej: '))
+        pokusy += 1
     elif n > guess:
         guess = int(input('Hadaj viac: '))
-print('Uhadol si cislo.')
+        pokusy += 1
+print('Uhadol si cislo.Travalo ti to',pokusy,'pokusov.')
